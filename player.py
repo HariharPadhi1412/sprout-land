963118135cff4749cd63af821b3ba06227cbb64c
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-08-05 15:50:30 +0530

    created conditions for checking whether a player is moving in which direction.

--- player.py
+++ player.py
@@ -11,17 +11,43 @@ class Player(pygame.sprite.Sprite):
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
 
+        # directions
+        self.directions = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-            print('up')
+            self.directions.y = -1
         elif keys[pygame.K_DOWN]:
-            print('down')
-        elif keys[pygame.K_RIGHT]:
-            print('right')
+            self.directions.y = 1
+        else:
+            self.directions.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.directions.x = 1
         elif keys[pygame.K_LEFT]:
-            print('left')
+            self.directions.x = -1
+        else:
+            self.directions.x = 0
+
+    def move(self, dt):
+
+        # normalizing the vector
+        if self.directions.magnitude() > 0:
+            self.directions = self.directions.normalize()
+
+        # horizontal movement
+        self.pos.x += self.directions.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        # vertical movement
+        self.pos.y += self.directions.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
 
     def update(self, dt):
         self.input()
+        self.move(dt)
