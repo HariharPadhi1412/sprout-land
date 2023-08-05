a5f6038918eccb68f8d558de1154e935e3dbe3bf
Harihar Padhi <39830449+1515236544hit@users.noreply.github.com>
2023-08-05 15:28:33 +0530

    created a player movement checker in the above file

--- player.py
+++ player.py
@@ -10,3 +10,18 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((64, 32))
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            print('up')
+        elif keys[pygame.K_DOWN]:
+            print('down')
+        elif keys[pygame.K_RIGHT]:
+            print('right')
+        elif keys[pygame.K_LEFT]:
+            print('left')
+
+    def update(self, dt):
+        self.input()
